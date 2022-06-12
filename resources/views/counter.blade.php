1d13c0ac044daa98e531b240f3e3b089a8593272
jks0586 <jks0586@gmail.com>
2022-06-12 17:44:10 +0530

    update

---
+++ resources/views/counter.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel-livewire-alpine</title>
+
+        @livewireStyles
+    </head>
+    <body class="antialiased">
+        <livewire:counter /> 
+    @livewireScripts
+    </body>
+    </html>
